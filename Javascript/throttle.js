f18daceee07ca65ced590a397a444aca6c601739
Luben Gonsalves <lubenfrank48@gmail.com>
2023-01-29 21:14:04 +0100

    refactor throttle

--- Javascript/throttle.js
+++ Javascript/throttle.js
@@ -1,11 +1,34 @@
 function throttle(fn, limit) {
-  let flag = true;
-  return function () {
-    if (flag) {
-      fn.apply(this, arguments);
-      setTimeout(function () {
-        flag = false;
-      }, limit);
+  let shouldThrottle = false;
+
+  return function (...args) {
+    if (shouldThrottle) {
+      return;
     }
+
+    shouldThrottle = true;
+    setTimeout(function () {
+      shouldThrottle = false;
+    }, wait);
+
+    func.apply(this, args);
   };
 }
+
+let i = 0;
+function increment() {
+  i++;
+}
+const throttledIncrement = throttle(increment, 100);
+
+// t = 0: Call throttledIncrement(). i is now 1.
+throttledIncrement(); // i = 1
+
+// t = 50: Call throttledIncrement() again.
+//  i is still 1 because 100ms have not passed.
+throttledIncrement(); // i = 1
+
+// t = 101: Call throttledIncrement() again. i is now 2.
+//  i can be incremented because it has been more than 100ms
+//  since the last throttledIncrement() call at t = 0.
+throttledIncrement(); // i = 2
