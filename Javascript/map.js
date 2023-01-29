bf6a78084aa9911f93f81614b39529aac1f92c8a
Luben Gonsalves <lubenfrank48@gmail.com>
2023-01-29 16:38:50 +0100

    refactor map

--- Javascript/map.js
+++ Javascript/map.js
@@ -1,14 +1,16 @@
 Array.prototype.customMap = function (callBack) {
-  let result = [];
+  let result = new Array(this.length);
 
   this.forEach((value, index, array) => {
-    result.push(callBack(value, index, array));
+    if (Object.hasOwn(this, index)) {
+      result[index] = callBack(value, index, array);
+    }
   });
 
   return result;
 };
 
-let array = [1, 2, 3, 4, 5];
+let array = [1, 2, 3, , 4, 5];
 
 let sqauredArray = array.customMap(function (val) {
   return val * 2;
