da98840b38ab90252da1acea5f4bf2702ba61f39
Luben Gonsalves <lubenfrank48@gmail.com>
2023-01-30 21:19:11 +0100

    promise all

---
+++ Javascript/promiseAll.js
@@ -0,0 +1,35 @@
+function promiseAll(iterable) {
+  return new Promise((resolve, reject) => {
+    const results = new Array(iterable.length);
+    let unresolved = iterable.length;
+
+    if (unresolved === 0) {
+      resolve(results);
+      return;
+    }
+
+    iterable.forEach(async (item, index) => {
+      try {
+        const value = await item;
+        results[index] = value;
+        unresolved -= 1;
+
+        if (unresolved === 0) {
+          resolve(results);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+}
+// Resolved example.
+const p0 = Promise.resolve(3);
+const p1 = 42;
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 100);
+});
+
+await promiseAll([p0, p1, p2]);
