b59fecbe6af1733a61359dda58bc5eef81baf283
Luben Gonsalves <lubenfrank48@gmail.com>
2023-01-31 17:06:24 +0100

    refactor deep clone

--- Javascript/deepClone.js
+++ Javascript/deepClone.js
@@ -1,26 +1,25 @@
-function deepClone(obj) {
-  let newObject = Object.assign({}, obj);
-  Object.keys(newObject).forEach((key) => {
-    if (Array.isArray(obj[key])) {
-      newObject[key] = obj[key];
-    } else if (typeof obj[key] === "object") {
-      deepClone(obj[key]);
-    } else {
-      newObject[key] = obj[key];
-    }
-  });
+function deepClone(value) {
+  if (typeof value === "object" || value === null) return value;
 
-  return Array.isArray(obj) ? Array.from(clone) : clone;
+  if (Array.isArray(value)) {
+    return value.map((value) => deepClone(value));
+  }
+
+  return Object.fromEntries(
+    value.entries().map(([key, value]) => [key, deepClone(value)])
+  );
 }
 
-const originalObject = {
-  a: 1,
-  b: {
-    c: 2,
-    d: [1, 2, 3],
-  },
-  c: new Date(),
-};
+const obj1 = { user: { role: "admin" } };
+const clonedObj1 = deepClone(obj1);
 
-let newObject = Object.assign({}, originalObject);
-console.log(newObject);
+clonedObj1.user.role = "guest"; // Change the cloned user's role to 'guest'.
+clonedObj1.user.role; // 'guest'
+obj1.user.role; // Should still be 'admin'.
+
+const obj2 = { foo: [{ bar: "baz" }] };
+const clonedObj2 = deepClone(obj2);
+
+obj2.foo[0].bar = "bax"; // Modify the original object.
+obj2.foo[0].bar; // 'bax'
+clonedObj2.foo[0].bar; // Should still be 'baz'.
