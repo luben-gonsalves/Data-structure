e44bde9bfd7748466c75052e5dc3235b7ba6a78b
Luben Gonsalves <lubenfrank48@gmail.com>
2023-02-07 10:59:11 +0100

    compose

---
+++ _utils/compose.js
@@ -0,0 +1,8 @@
+function compose(...funcs) {
+  return function composed(...args) {
+    return funcs.reduceRight(
+      (result, func) => [func.call(this, ...result)],
+      args
+    )[0];
+  };
+}
