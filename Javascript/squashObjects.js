63e6eb8f0afe3108d050914fc34e31c7a1b23562
Luben Gonsalves <lubenfrank48@gmail.com>
2023-02-19 00:11:09 +0100

    squash objects

---
+++ Javascript/squashObjects.js
@@ -0,0 +1,11 @@
+export default function squashObject(object, path = [], output = {}) {
+  for (const [key, value] of Object.entries(object)) {
+    if (typeof value !== "object" || value === null) {
+      output[path.concat(key).filter(Boolean).join(".")] = value;
+    } else {
+      squashObject(value, path.concat(key), output);
+    }
+  }
+
+  return output;
+}
